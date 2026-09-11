0581d1811795325f22b2a195bdd82479b6b2440f
MAX ONYX <46382297+magzim21@users.noreply.github.com>
2026-09-11 16:38:26 -0700

    chore: remove temporary landing generator test

--- src/generate-test.mjs
+++
@@ -1,5 +0,0 @@
-import { mkdir, readFile, writeFile } from "node:fs/promises";
-
-const html = await readFile("index.html", "utf8");
-await mkdir("used-car-reset", { recursive: true });
-await writeFile("used-car-reset/index.html", html, "utf8");
